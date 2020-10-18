fd2a0280ceeaf21d1286766a9c544f65274fdb20
Gabriel Uccello <gabrielucc@gmail.com>
2020-10-17 23:16:34 -0300

    new style and structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  Navbar,
+  Nav,
+  Container,
+  Row,
+  Col,
+  Card,
+  Button,
+} from "react-bootstrap";
+// import { Container, Row } from "reactstrap";
 
 function App() {
+  const { products, setProductos } = useState([]);
+
+  useEffect(() => {
+    getProductos();
+  }, []);
+
+  function getProductos() {
+    fetch("http://localhost:8080/")
+      .then((response) => response.json())
+      .then((data) => {
+        setProductos(data.products.products);
+        console.log(products);
+      })
+      .catch(() => this.setState({}));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="light" variant="light">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Inicio</Nav.Link>
+          <Nav.Link href="#features">Comprar</Nav.Link>
+          <Nav.Link href="#pricing">Blog</Nav.Link>
+        </Nav>
+        <Navbar.Brand href="#home">NOMADS</Navbar.Brand>
+      </Navbar>
+      <Container>
+        <Row className="text-center">
+          <Col>
+            <h2>Productos</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs md lg="2">
+            <h2>Menu</h2>
+          </Col>
+          <Col xs md lg="10">
+            {/* {products.map((product) => (
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>{product.name}</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            ))} */}
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
