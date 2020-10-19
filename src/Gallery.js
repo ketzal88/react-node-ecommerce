1ba76f508cfdabdaa74e9dd093541dfbbb7b4553
Gabriel Uccello <gabrielucc@gmail.com>
2020-10-19 10:56:37 -0300

    added pagination

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import {
-  Navbar,
-  Nav,
   Container,
   Row,
   Col,
@@ -11,6 +9,7 @@ import {
   Button,
   Dropdown,
   Form,
+  Pagination,
 } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import Header from "./Header";
@@ -18,21 +17,25 @@ import Header from "./Header";
 function Gallery() {
   const [products, setProductos] = useState([]);
   const [search, setSearch] = useState("");
+  const [selectedPage, setSelectedPage] = useState(0);
+  const [page, setPage] = useState([]);
   const [min, setMin] = useState("");
   const [max, setMax] = useState("");
 
   useEffect(() => {
     getProductos();
-  }, []);
+  }, [selectedPage]);
 
   const history = useHistory();
   const handleClick = (product) => history.push(`/product/${product}`);
 
   function getProductos() {
-    fetch("http://localhost:8080/products")
+    fetch(`http://localhost:8080/products?page=${selectedPage}`)
       .then((response) => response.json())
       .then((data) => {
-        setProductos(data.products.products);
+        setProductos(data.productos);
+        const pag = Array.from(Array(data.pages).keys());
+        setPage(pag);
       });
   }
 
@@ -60,6 +63,7 @@ function Gallery() {
       });
   };
 
+  console.log(selectedPage);
   return (
     <div>
       <Header />
@@ -120,6 +124,25 @@ function Gallery() {
             </Form>
           </Col>
           <Col xs md lg="9">
+            <Row>
+              <Pagination
+                style={{
+                  maxWidth: "25%",
+                  padding: "1em",
+                  border: "none",
+                }}
+              >
+                {page.map((pag, index) => (
+                  <Pagination.Item
+                    key={index}
+                    onClick={() => setSelectedPage(index)}
+                    // active={selectedPage}
+                  >
+                    {index + 1}
+                  </Pagination.Item>
+                ))}
+              </Pagination>
+            </Row>
             <Row>
               {products.map((product) => (
                 <Card
